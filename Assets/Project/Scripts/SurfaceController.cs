2310bd0bb55aa50799c6699f47d4544f60c35ffc
Tugushev Damir Rustamovich <0damir.1tugushev@gmail.com>
2024-10-26 19:55:21 +0300

    Store friction type in surface controller

--- Assets/Project/Scripts/SurfaceController.cs
+++ Assets/Project/Scripts/SurfaceController.cs
@@ -5,7 +5,9 @@ using UnityEngine;
 // Completely new class, no source provided
 public class SurfaceController : MonoBehaviour
 {
-    public QuantityDynamicsType dynamicsType;
+    [Header("Surface types")] // Using existing class from "Minimalist Quantity System"
+    public QuantityDynamicsType frictionType;
+    public QuantityDynamicsType temperatureType;
 
     private void OnCollisionStay(Collision ball)
     {
@@ -13,10 +15,10 @@ public class SurfaceController : MonoBehaviour
         var temperature = ball.transform.parent
             .gameObject.GetComponent<QuantityController>().temperature;
 
-        if (dynamicsType.Equals(QuantityDynamicsType.None))
-            temperature.PassiveDynamics.Type = Math.Round(temperature.Amount, 2) == 0 ? dynamicsType
+        if (temperatureType.Equals(QuantityDynamicsType.None))
+            temperature.PassiveDynamics.Type = Math.Round(temperature.Amount, 2) == 0 ? temperatureType
                 : (temperature.Amount > 0 ? QuantityDynamicsType.Depletion : QuantityDynamicsType.Accumulation);
-        else temperature.PassiveDynamics.Type = dynamicsType;
+        else temperature.PassiveDynamics.Type = temperatureType;
     }
 
     private void OnCollisionExit(Collision ball)
