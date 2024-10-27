0c9a0af04842365043500a7ab0f91fe563588567
Tugushev Damir Rustamovich <0damir.1tugushev@gmail.com>
2024-10-27 18:19:05 +0300

    Move surface friction logic into a separate state machine

--- Assets/Project/Scripts/SurfaceController.cs
+++ Assets/Project/Scripts/SurfaceController.cs
@@ -6,38 +6,7 @@ using UnityEngine;
 public class SurfaceController : MonoBehaviour
 {
     [Header("Surface types")] // Using existing class from "Minimalist Quantity System"
-    public QuantityDynamicsType frictionType;
     public QuantityDynamicsType temperatureType;
-
-    /* Source of method:
-     * https://discussions.unity.com/t/is-there-a-way-to-alter-friction-and-bounciness-
-     * from-a-script-that-is-a-component-of-a-gameobject-that-uses-them/253091/2 */
-    private void Start()
-    {
-        var surface = GetComponent<Collider>();
-        if (!surface) return;
-        var material = surface.material;
-        
-        // Change #1: Set friction based on surface type
-        switch (frictionType)
-        {
-            case QuantityDynamicsType.Accumulation:
-                UpdateFriction(material, 0.75f);
-                break;
-            case QuantityDynamicsType.None:
-                UpdateFriction(material, 0.5f);
-                break;
-            case QuantityDynamicsType.Depletion:
-                UpdateFriction(material, 0.25f);
-                break;
-        }
-    }
-
-    private static void UpdateFriction(PhysicMaterial material, float friction)
-    {
-        material.staticFriction = friction;
-        material.dynamicFriction = friction;
-    }
     
     private void OnCollisionStay(Collision ball)
     {
