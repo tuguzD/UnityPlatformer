7cd429c1e03361655850b400edf57936bda9cc23
Tugushev Damir Rustamovich <0damir.1tugushev@gmail.com>
2024-10-28 12:26:21 +0300

    Rename variable, add test info in `QuantityController` (in case I'll get tired of managing VS application variables)

--- Assets/Project/Scripts/QuantityController.cs
+++ Assets/Project/Scripts/QuantityController.cs
@@ -3,6 +3,16 @@ using UnityEngine;
 
 public class QuantityController : MonoBehaviour
 {
+    // [Header("Friction parameters")]
+    // public float frictionBase = 0.5f;
+    // public float frictionDelta = 0.25f;
+    //
+    // [Header("Temperature parameters")]
+    // public QuantityBhv temperature;
+    // public float temperatureLowerBound = 0.25f;
+    // public float temperatureUpperBound = 0.75f;
+    // public float temperatureDelta = 0.05f;
+    
     [Header("Subjective Quantities")] // Example from PlayerBhv.cs
     public QuantityBhv temperature;
     public QuantityBhv durability;
