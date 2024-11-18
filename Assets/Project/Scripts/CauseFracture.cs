a17d6529aab23f1708c24b1e8c3b7827dafc01b0
Tugushev Damir Rustamovich <0damir.1tugushev@gmail.com>
2024-11-18 13:33:20 +0300

    Calculate collision force to fracture breakable obstacle

--- Assets/Project/Scripts/CauseFracture.cs
+++ Assets/Project/Scripts/CauseFracture.cs
@@ -5,7 +5,7 @@ public class CauseFracture : MonoBehaviour
 {
     [Tooltip("Minimum contact collision force required to cause the object to fracture.")]
     public float minimumCollisionForce = 500f;
-    
+
     private Fracture _fracture;
 
     private void Start()
@@ -16,11 +16,23 @@ public class CauseFracture : MonoBehaviour
     private void OnCollisionEnter(Collision ball)
     {
         if (!ball.gameObject.CompareTag("Player")) return;
-        
-        var collisionForce = ball.impulse.magnitude / Time.fixedDeltaTime;
-        // Debug.Log(collisionForce);
-
-        if (collisionForce > minimumCollisionForce)
+        if (CollisionForce(ball) > minimumCollisionForce)
             _fracture.CauseFracture();
+    }
+
+    private static float CollisionForce(Collision ball)
+    {
+        var quantities = ball.gameObject.transform.parent.GetComponent<QuantityController>();
+
+        var collisionForce = ball.impulse.magnitude / Time.fixedDeltaTime;
+        var spikinessMultiplier = 1 + quantities.spikiness.Amount;
+
+        var x = quantities.plasticity.Amount;
+        // how well energy is transferred to the object - the closer it is to average, the better
+        var plasticityMultiplier = 0.5f + (x < 0.5 ? 2*x : 2*(1 - x));
+
+        var result = collisionForce * spikinessMultiplier * plasticityMultiplier;
+        Debug.Log($"{collisionForce} * {spikinessMultiplier} * {plasticityMultiplier} = {result}");
+        return result;
     }
 }
