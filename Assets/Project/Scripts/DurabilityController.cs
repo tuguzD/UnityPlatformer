c17fb1720be4338964f27bbf65ecd938ac595e11
Tugushev Damir Rustamovich <0damir.1tugushev@gmail.com>
2024-11-21 19:15:07 +0300

    Figure out how to damage ball when hitting both floor from the height or obstacle ahead of it, fix durability of breakable obstacles

--- Assets/Project/Scripts/DurabilityController.cs
+++ Assets/Project/Scripts/DurabilityController.cs
@@ -4,6 +4,19 @@ public class DurabilityController : MonoBehaviour
 {
     private void OnCollisionEnter(Collision collision)
     {
-        Debug.LogWarning(collision.gameObject.name);
+        if (collision.gameObject.CompareTag("PickUp")) return;
+        foreach (var contact in collision.contacts)
+        {
+            var result = Vector3.Scale(collision.relativeVelocity, contact.normal);
+            // combine both height (y) and forward speed (z)
+            var power = Mathf.Abs(result.y) + Mathf.Abs(result.z);
+            if (collision.gameObject.CompareTag("Break")) power /= 3;
+
+            // apply "damage" to the player ball
+            if (power > 5)
+            {
+                Debug.Log($"Hit {collision.gameObject.name} with power {power}");
+            }
+        }
     }
 }
