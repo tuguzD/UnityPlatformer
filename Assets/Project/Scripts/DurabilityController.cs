d84a1a5c7d373059b4f68b2d73eaa02bf8176b75
Tugushev Damir Rustamovich <0damir.1tugushev@gmail.com>
2024-11-21 23:27:30 +0300

    Apply damage to player ball colliding with both floor and obstacles

--- Assets/Project/Scripts/DurabilityController.cs
+++ Assets/Project/Scripts/DurabilityController.cs
@@ -2,21 +2,37 @@ using UnityEngine;
 
 public class DurabilityController : MonoBehaviour
 {
+    public float threshold = 10;
+
+    private QuantityController _quantities;
+
+    private void Start()
+    {
+        _quantities = GetComponentInParent<QuantityController>();
+    }
+
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.CompareTag("PickUp")) return;
-        foreach (var contact in collision.contacts)
-        {
-            var result = Vector3.Scale(collision.relativeVelocity, contact.normal);
-            // combine both height (y) and forward speed (z)
-            var power = Mathf.Abs(result.y) + Mathf.Abs(result.z);
-            if (collision.gameObject.CompareTag("Break")) power /= 3;
 
-            // apply "damage" to the player ball
-            if (power > 5)
-            {
-                Debug.Log($"Hit {collision.gameObject.name} with power {power}");
-            }
-        }
+        var relativeVelocity = Vector3.Scale(collision.relativeVelocity, collision.contacts[0].normal);
+        // combine both height (y) and forward speed (z)
+        var damageVelocity = Mathf.Abs(relativeVelocity.y) + Mathf.Abs(relativeVelocity.z);
+        if (collision.gameObject.CompareTag("Break")) damageVelocity /= 2;
+
+        if (damageVelocity < threshold) return;
+        // apply "damage" to the player ball
+        Debug.Log($"Hit {collision.gameObject.name} with power {damageVelocity}");
+        _quantities.durability.Amount -= DamageApplied(damageVelocity);
+    }
+
+    private float DamageApplied(float velocity)
+    {
+        var mass = threshold * _quantities.size.Amount;
+        // max plasticity equals min brittleness, and vice versa
+        var brittleness = 1 - _quantities.plasticity.Amount;
+        
+        var result = velocity * brittleness / mass;
+        return result;
     }
 }
